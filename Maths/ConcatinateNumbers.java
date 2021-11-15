28e45b416a3e1fcc493e332b0c1fb92a0caacfb9
Vishal Hulawale <hulawale.vishal@gmail.com>
2021-11-15 20:20:41 +0530

    Arrray - Maths

---
+++ Maths/ConcatinateNumbers.java
@@ -0,0 +1,85 @@
+
+/*
+Concatenate Three Numbers
+Problem Description
+
+Given three 2-digit integers, A, B and C, find out the minimum number that can be obtained by concatenating them in any order.
+
+Return the minimum result that can be obtained.
+
+
+
+Problem Constraints
+10 <= A, B, C <= 99
+
+
+
+Input Format
+The first argument of input contains an integer, A.
+
+The second argument of input contains an integer, B.
+
+The third argument of input contains an integer, C.
+
+
+
+Output Format
+Return an integer, representing the answer.
+
+
+
+Example Input
+Input 1:
+
+ A = 10
+ B = 20
+ C = 30
+Input 2:
+
+ A = 55
+ B = 43
+ C = 47 
+
+
+Example Output
+Output 1:
+
+ 102030 
+Output 2:
+
+ 434755 
+
+
+Example Explanation
+Explanation 1:
+
+ 10 + 20 + 30 = 102030 
+Explanation 2:
+
+ 43 + 47 + 55 = 434755 
+*/
+import java.util.Arrays;
+
+class ConcatinateNumbers {
+
+    public static int solve(int A, int B, int C) {
+
+        int[] num = { A, B, C };
+
+        Arrays.sort(num);
+
+        String str = "";
+        for (int i = 0; i < num.length; i++) {
+            str += num[i];
+        }
+
+        return Integer.parseInt(str);
+    }
+
+    public static void main(String[] args) {
+        int num = solve(10, 20, 30);
+
+        System.out.println(num);
+    }
+
+}
---
+++ Maths/ExcelColumn.java
@@ -0,0 +1,74 @@
+/*
+Excel Column Number
+Problem Description
+
+Given a column title as appears in an Excel sheet, return its corresponding column number.
+
+
+
+Problem Constraints
+1 <= length of the column title <= 5
+
+
+
+Input Format
+Input a string which represents the column title in excel sheet.
+
+
+
+Output Format
+Return a single integer which represents the corresponding column number.
+
+
+
+Example Input
+Input 1:
+
+ AB
+Input 2:
+
+ ABCD
+
+
+Example Output
+Output 1:
+
+ 28
+Output 2:
+
+ 19010
+
+
+Example Explanation
+Explanation 1:
+
+ A -> 1
+ B -> 2
+ C -> 3
+ ...
+ Z -> 26
+ AA -> 27
+ AB -> 28
+ */
+ 
+ class ExcelColumn{
+ 	public static int solve(String A){
+        	int ans = 0;
+ 		int N = A.length();
+ 		int exp = 1;
+ 		
+ 		for(int i = N-1; i >= 0; i--){
+ 			int charVal = ((int) A.charAt(i)) - 64;	
+ 			ans += charVal * exp;
+ 			
+ 			exp *= 26; 
+ 		}
+ 		
+ 		return ans;
+ 	}
+ 	
+ 	public static void main(String[] args){
+ 		System.out.println(solve("AB")); 	
+ 	}
+ 
+ }
