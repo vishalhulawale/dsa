e773bcb351b1d8043b362d9e9d05810e69789fc9
Vishal Hulawale <hulawale.vishal@gmail.com>
2021-11-25 23:59:58 +0530

    Maths - LCM and Overlapping Rectangle

---
+++ Maths/LCM.java
@@ -0,0 +1,92 @@
+/*
+Least Common Multiple
+Problem Description
+
+You are given two non-negative integers A and B, find the value of Least Common Multiple (LCM) of A and B.
+
+LCM of two integers is the smallest positive integer that is divisible by both.
+
+
+
+Problem Constraints
+1 <= A, B <= 1000
+
+
+
+Input Format
+First argument is an integer A.
+
+Second argument is an integer B.
+
+
+
+Output Format
+Return an integer.
+
+
+
+Example Input
+Input 1:
+
+ A = 2
+ B = 3 
+Input 2:
+
+ A = 9
+ B = 6
+Input 3:
+
+ A = 2
+ B = 6
+
+
+Example Output
+Output 1:
+
+ 6 
+Input 2:
+
+ 18 
+Input 3:
+
+ 6 
+
+
+Example Explanation
+Explanation 1:
+
+ 6 is the smallest positive integer which is divisible by both 2 (2 * 3 = 6) and 3 (3 * 2 = 6).
+Explanation 2:
+
+ 18 is the smallest positive integer which is divisible by both 9 (9 * 2 = 18) and 6 (6 * 3 = 18).
+Explanation 3:
+
+ 6 is the smallest positive integer which is divisible by both 2 (2 * 3 = 6) and 6 (6 * 1 = 6).
+*/
+package Maths;
+
+class LCM {
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+
+    public static long lcm(int a, int b) {
+        long product = a * b;
+        int gcd = gcd(a, b);
+
+        return product / gcd;
+    }
+
+    public static void main(String args[]) {
+        int a = 2, b = 3;
+
+        System.out.println(lcm(a, b));
+    }
+}
