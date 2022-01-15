34870dd9846e6c1b97b62649aa304746ef95af6e
Vishal Hulawale <hulawale.vishal@gmail.com>
2022-01-15 22:42:08 +0530

    Recursion - Factorial

---
+++ Recursion/Factorial.java
@@ -0,0 +1,62 @@
+/*
+Find Factorial!
+
+
+Problem Description
+Write a program to find the factorial of the given number A.
+
+
+Problem Constraints
+0 <= A <= 12
+
+
+Input Format
+First and only argument is an integer A.
+
+
+Output Format
+Return an integer denoting the factorial of the number A.
+
+
+Example Input
+Input 1:
+A = 4
+
+Input 2:
+A = 1
+
+
+Example Output
+Output 1:
+24
+
+Output 2:
+1
+
+
+Example Explanation
+Explanation 1:
+Factorial of 4 = 4 * 3 * 2 * 1 = 24
+
+Explanation 2:
+Factorial of 1 = 1
+*/
+
+package Recursion;
+
+public class Factorial {
+
+    public static int solve(int A) {
+
+        if (A == 1)
+            return 1;
+
+        return A * solve(A - 1);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solve(4));
+    }
+
+}
