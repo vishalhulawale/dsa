f95abf27ceee18f2162dbe80b6410d286aa1d12f
Vishal Hulawale <hulawale.vishal@gmail.com>
2022-07-02 08:37:05 +0530

    Problem Solving - Least two greater elements

--- ProblemSolving/LeastTwoGreaterElements.java
+++ ProblemSolving/LeastTwoGreaterElements.java
@@ -1,64 +1,58 @@
-/*
+/**
  * Elements which have at-least two greater elements
-Problem Description
-
-You are given an array of distinct integers A, you have to find and return all elements in array which have at-least two greater elements than themselves.
-
-NOTE: The result should have the order in which they are present in the original array.
-
-
-
-Problem Constraints
-3 <= |A| <= 105
-
--109 <= A[i] <= 109
-
-
-
-Input Format
-First and only argument is an integer array A.
-
-
-
-Output Format
-Return an integer array containing the elements of A which have at-least two greater elements than themselves in A.
-
-
-
-Example Input
-Input 1:
-
- A = [1, 2, 3, 4, 5]
-Input 2:
-
- A = [11, 17, 100, 5]
-
-
-Example Output
-Output 1:
-
- [1, 2, 3]
-Output 2:
-
- [11, 5]
-
-
-Example Explanation
-Explanation 1:
-
- Number of elements greater than 1: 4
- Number of elements greater than 2: 3
- Number of elements greater than 3: 2
- Number of elements greater than 4: 1
- Number of elements greater than 5: 0
- Elements 1, 2 and 3 have atleast 2 elements strictly greater than themselves.
-Explanation 2:
-
- Number of elements greater than 11: 2
- Number of elements greater than 17: 1
- Number of elements greater than 100: 0
- Number of elements greater than 5: 3
- Elements 5 and 11 have atleast 2 elements strictly greater than themselves.
+ * 
+ * Problem Description:
+ * You are given an array of distinct integers A, you have to find and  
+ * return all elements in array which have at-least two greater elements than themselves.
+ * 
+ * NOTE: The result should have the order in which they are present in the original array.
+ * 
+ * 
+ * Problem Constraints:
+ * 3 <= |A| <= 10^5
+ * -10^9 <= A[i] <= 10^9
+ * 
+ * 
+ * Input Format:
+ * First and only argument is an integer array A.
+ * 
+ * 
+ * Output Format:
+ * Return an integer array containing the elements of A which have at-least two greater elements than themselves in A.
+ * 
+ * 
+ * Example Input
+ * Input 1:
+ * A = [1, 2, 3, 4, 5]
+ * 
+ * Input 2:
+ * A = [11, 17, 100, 5]
+ * 
+ * Example Output
+ * Output 1:
+ * [1, 2, 3]
+ * 
+ * Output 2:
+ * [11, 5]
+ * 
+ * 
+ * Example Explanation
+ * Explanation 1:
+ * Number of elements greater than 1: 4
+ * Number of elements greater than 2: 3
+ * Number of elements greater than 3: 2
+ * Number of elements greater than 4: 1
+ * Number of elements greater than 5: 0
+ * Elements 1, 2 and 3 have atleast 2 elements strictly greater than themselves.
+ * 
+ * Explanation 2:
+ * Number of elements greater than 11: 2
+ * Number of elements greater than 17: 1
+ * Number of elements greater than 100: 0
+ * Number of elements greater than 5: 3
+ * Elements 5 and 11 have atleast 2 elements strictly greater than themselves.
+ * 
+ * Solution: Find two largest elements, omit those from array and return array.
  */
 package ProblemSolving;
 
@@ -81,9 +75,6 @@ class LeastTwoGreaterElements {
                 secondLargest = num;
             }
         }
-
-        System.out.println(largest);
-        System.out.println(secondLargest);
 
         ArrayList<Integer> result = new ArrayList<Integer>();
 
@@ -116,9 +107,8 @@ class LeastTwoGreaterElements {
         for (int num : arr)
             input.add(num);
 
-        // ArrayList<Integer> result = solve(input);
-
-        // print(result);
+        ArrayList<Integer> result = solve(input);
+        print(result);
     }
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ds-algorithms
 Data structures and Algoriths examples implemented using java
+
+## Problem Solving
+1. [Elements which have at-least two greater elements](https://github.com/vishalhulawale/dsa/blob/048392098fb0f4e14422e07edfec53f1f2e4058e/ProblemSolving/LeastTwoGreaterElements.java)
+1. [Minimum Picks]()
+1. [Smaller and Greater]()
+1. [Pattern Printing -1]()
+
+[Square Root](https://www.mathsisfun.com/square-root.html), [Logarithms](https://www.mathsisfun.com/algebra/logarithms.html)
